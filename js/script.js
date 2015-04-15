fe75660d7ab7588ec5823336926e2a407a44ee66
Mark Harris <harrisdm@hotmail.com>
2015-04-15 23:26:28 +1000

    Added Comments

--- js/script.js
+++ js/script.js
@@ -1,48 +1,70 @@
+/********************
 
+	script.js
+	Mark Harris - 4/15/2015
+ 
+	JS/Jquery Etch-A-Sketch Project for
+	http://www.theodinproject.com/web-development-101/javascript-and-jquery
+
+********************/
 
 $(document).ready(function() {
 	
-	sketch(16);
+	// the default sketch grid and effect
+	sketch(1,16);
 	
-	$("#bw").click(function() {	sketch(null, 1); });
-	$("#color").click(function() { sketch(null, 2); });
-	$("#opacity").click(function() { sketch(null, 3); });
+	// change the effect when sketching
+	$("#bw").click(function() {	sketch(1); });
+	$("#color").click(function() { sketch(2); });
+	$("#opacity").click(function() { sketch(3); });
 	
 	
-	function sketch(size, option) {
+	function sketch(option, size) {
 		
+		// if the size isn't set, ask user for size
 		size = size || prompt("How many blocks wide?");
-		option = option || 1;
 		
+		// create a grid of width and heigh [size]
 		make_grid(size);
+		
 		
 		$(".box").mouseenter(function(){
 			switch(option) {
-				case 1:		$(this).addClass("box_lit");	
+				case 2:		// random colour effect
+							var randomColor = Math.floor(Math.random()*16777215).toString(16);								// create a random hex value
+							$(this).css('background-color', "#" + randomColor);												// change the css of the box
+							break;																
+				case 3:		// increasing opacity effect
+							$(this).addClass("box_lit");																	// assign the box the [box_lit] class to add colour
+							var currentOpacity = $(this).css("opacity");													// find the current opacity of the box
+							$(this).css("opacity", ( currentOpacity > 0 ) ? ( currentOpacity - 0.1 ) : currentOpacity );	// change the opacity of the box
 							break;
-				case 2:		var randomColor = Math.floor(Math.random()*16777215).toString(16);
-							$(this).css('background-color', "#" + randomColor);	
-							break;
-				case 3:		$(this).addClass("box_lit");
-							var currentOpacity = $(this).css("opacity");
-							$(this).css("opacity", ( currentOpacity > 0 ) ? ( currentOpacity - 0.1 ) : currentOpacity );
-							break;
-				default:	$(this).addClass("box_lit");	
+				default:	// black & white effect
+							$(this).addClass("box_lit");	
 			}
 		});
 	}
 	
 	
+	// create a new grid of the desired size
 	function make_grid(numSquares) {
-		$("#sketchContainer").empty();
-				
+		
+		// delete the current grid
+		$("#gridContainer").empty();
+		// option 2 =  $("gridContainer").html("");
+		// option 3 =  $("gridContainer").delete(".box");
+		
+		// iterate through loop adding boxes to the gridContainer
 		for(var i=0; i<numSquares; i++) {
 			for(var j=0; j<numSquares; j++) {
-				$("#sketchContainer").append("<div class='box'></div>");
+				$("#gridContainer").append("<div class='box'></div>");
 			}
-			$("#sketchContainer").append("<div class='end_row'></div>");
+			// This forces a new line so that [size] rounding doesn't result in extra boxes on a row
+			$("#gridContainer").append("<div class='end_row'></div>");
 		}
-		var square_size = $('#sketchContainer').width()/numSquares - 2;	// The -2 accounts for border size
+		
+		// calculate and set the box width and height
+		var square_size = $('#gridContainer').width()/numSquares - 2;															// The -2 accounts for border size
 		$(".box").height(square_size);
 		$(".box").width(square_size);
 	}
