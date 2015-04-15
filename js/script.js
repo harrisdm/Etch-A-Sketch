e7253c2b0d14d2680ae09cf75b58b81327d92fa9
Mark Harris <harrisdm@hotmail.com>
2015-04-15 22:45:38 +1000

    Condensed Code
    Removed function repetition by consolidating the three effects into a
    switch statement

--- js/script.js
+++ js/script.js
@@ -1,63 +1,51 @@
 
 
-
 $(document).ready(function() {
 	
-	make_grid(16);
-	bw_sketch();
+	sketch(16);
 	
-	$("#bw").click(function() {
-		var size = prompt("How many blocks wide?");
-		make_grid(size);
-		bw_sketch();
-	});
+	$("#bw").click(function() {	sketch(null, 1); });
+	$("#color").click(function() { sketch(null, 2); });
+	$("#opacity").click(function() { sketch(null, 3); });
 	
-	$("#color").click(function() {
-		var size = prompt("How many blocks wide?");
-		make_grid(size);
-		color_sketch();
-	});
-	$("#opacity").click(function() {
 	
-		var size = prompt("How many blocks wide?");
+	function sketch(size, option) {
+		
+		size = size || prompt("How many blocks wide?");
+		option = option || 1;
+		
 		make_grid(size);
-		opacity_sketch();
-	});
+		
+		$(".box").mouseenter(function(){
+			switch(option) {
+				case 1:		$(this).addClass("box_lit");	
+							break;
+				case 2:		var randomColor = Math.floor(Math.random()*16777215).toString(16);
+							$(this).css('background-color', "#" + randomColor);	
+							break;
+				case 3:		$(this).addClass("box_lit");
+							var currentOpacity = $(this).css("opacity");
+							$(this).css("opacity", ( currentOpacity > 0 ) ? ( currentOpacity - 0.1 ) : currentOpacity );
+							break;
+				default:	$(this).addClass("box_lit");	
+			}
+		});
+	}
 	
 	
 	function make_grid(numSquares) {
-		$("#container").empty();
+		$("#sketchContainer").empty();
 				
 		for(var i=0; i<numSquares; i++) {
 			for(var j=0; j<numSquares; j++) {
-				$("#container").append("<div class='box'></div>");
+				$("#sketchContainer").append("<div class='box'></div>");
 			}
-			$("#container").append("<div class='end_row'></div>");
+			$("#sketchContainer").append("<div class='end_row'></div>");
 		}
-		var square_size = $('#container').width()/numSquares - 2;	// The -2 accounts for border size
+		var square_size = $('#sketchContainer').width()/numSquares - 2;	// The -2 accounts for border size
 		$(".box").height(square_size);
 		$(".box").width(square_size);
 	}
 	
-	function bw_sketch() {
-		$(".box").mouseenter(function(){
-			$(this).addClass('square_lit');		
-		});
-	}
-	
-	function color_sketch() {
-		$(".box").mouseenter(function(){
-			var randomColor = Math.floor(Math.random()*16777215).toString(16);
-			$(this).css('background-color', "#" + randomColor);		
-		});
-	}
-	
-	function opacity_sketch() {
-		$(".box").mouseenter(function(){			
-			$(this).addClass('square_lit');
-			var currentOpacity = $(this).css("opacity");
-			$(this).css("opacity", ( currentOpacity > 0 ) ? ( currentOpacity - 0.1 ) : currentOpacity );
-		});
-	}
-	
 });
+
